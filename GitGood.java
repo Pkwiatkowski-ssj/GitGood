b8e186244732bf3d5d4f95c3f64aa7a56838634f
pkwiatkowski-ssj <pkwiatkowski@quinnipiac.edu>
2025-02-24 19:52:52 -0500

    Added basic a for loop

---
+++ GitGood.java
@@ -0,0 +1,9 @@
+public class GitGood {
+
+    public static void main(String[] args) {
+        // loop that prints 1 - 5
+        for (int i =1; i <= 5; i++) {
+            System.out.println("Count: " +i);
+        }
+    }
+}
